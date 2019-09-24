d686130995d2e00fdd4081aae0c6da2f56b30104
passionate-optimization-7504 <nicholasjardine9@gmail.com>
2019-09-24 18:44:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,7 @@ def reverse_each_word(sentence)
   string_arr = sentence.split
   reverse_word_arr =[]
   string_arr.collect { |word|
-    reverser_word_arr << word.reverser_word_arr
+    reverse_word_arr << word.reverse
   }
-  reverser_word_arr.join(" ")
+  reverse_word_arr.join(" ")
 end
